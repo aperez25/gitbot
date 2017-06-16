78bf7e31a944083a75c40648138ff6e44edfd039
aperez25 <alice.perez25@gmail.com>
2017-06-16 16:28:25 -0700

    chore: debugging

--- client/index.js
+++ client/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import Root from './root'
-import '../public/index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
