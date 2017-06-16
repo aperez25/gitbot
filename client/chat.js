8341a5a28c26cc83df6579ce624c5cfa708f02dd
aperez25 <alice.perez25@gmail.com>
2017-06-16 10:53:54 -0700

    chore: refactoring

--- client/chat.js
+++ client/chat.js
@@ -4,8 +4,8 @@ const Chat = () => (
   <div>
     <iframe
       align="bottom"
-      width="350"
-      height="430"
+      width="500"
+      height="600"
       src="https://console.api.ai/api-client/demo/embedded/b77433ff-768e-41ff-a8a7-24fe2a5219b8" />
   </div>
 )
