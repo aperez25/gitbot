71434a110c06b5e53afeb542051404a10f82128a
aperez25 <alice.perez25@gmail.com>
2017-06-19 02:23:02 -0700

    feat(slack: new slash command git/popular

--- server/api/slack.js
+++ server/api/slack.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const octokat = require('octokat')
-
+const moment = require('moment')
 const gitHubId = process.env.GH_CLIENT_ID
 const gitHubSecret = process.env.GH_CLIENT_SECRET
 const gitHubToken = process.env.GH_TOKEN
@@ -44,8 +44,10 @@ router
 	userName = gitRequest[0],
 	repoName = gitRequest[1],
 	branches = [],
+	formattedBranches = []
 	// fetches the repo's commit history
 	repo = octo.repos(userName, repoName)
+
 	repo.git.refs.fetch()
 	//get the data we need
 	.then(response => {
@@ -64,10 +66,10 @@ router
 			.then(object => {
 				index = branches.indexOf(object.sha)
 				branches[index].url = object.html_url
+				formattedBranches = branches.map(branch =>
+			`<${branch.url}|${branch.name}>`)
 			})
 			.catch(next)
-			return branches.map(branch =>
-			`<${branch.url}|${branch.name}>`)
 		})
 	})
 	.then(branchURLs => {
@@ -104,7 +106,6 @@ router
 		for (var i = 1; i <= 5; i++) {
 			const item = ({
 				fullName: searchResults.items[i].fullName,
-				// homepage: searchResults.items[i].homepage,
 				htmlUrl: searchResults.items[i].htmlUrl,
 				forks: searchResults.items[i].forks,
 				language: searchResults.items[i].language,
@@ -115,6 +116,37 @@ router
 		}
 
 		const searchText = `Here are the first five results for your search *${req.body.text}*:\n${searchItems.join('\n')}\n<${gitHubSearchURL}|Search through all results here.>`
+		// response back to Slack
+		res.send({text: searchText, channel: slackChannel, response_type: 'in_channel'})
+	})
+	.catch(next)
+})
+
+.post('/popular', (req, res, next) => {
+	const slackChannel = req.body.channel_id,
+	last7 = moment().subtract(7, 'days').calendar();
+	let gitHubSearchURL = `https://github.com/search?utf8=%E2%9C%93&type=Repositories&q=created:${last7}`
+
+	octo.search.repositories.fetch({
+		q: `created:${last7}`,
+		sort: 'stars',
+		order: desc
+	})
+	.then(searchResults => {
+		const searchItems = []
+		for (var i = 1; i <= 5; i++) {
+			const item = ({
+				fullName: searchResults.items[i].fullName,
+				htmlUrl: searchResults.items[i].htmlUrl,
+				forks: searchResults.items[i].forks,
+				language: searchResults.items[i].language,
+				lastUpdated: new Date(searchResults.items[i].updatedAt),
+				unixDate: new Date(searchResults.items[i].updatedAt).getTime() / 1000
+			})
+			searchItems.push(`${i}. <${item.htmlUrl}|${item.fullName}>: ${item.forks} forks,  language: ${item.language}, last updated: <!date^${item.unixDate}^{date_short_pretty}|${item.lastUpdated}>`)
+		}
+
+		const searchText = `The five most popular projects in the last week are: *${req.body.text}*:\n${searchItems.join('\n')}\n<${gitHubSearchURL}|Search through all results here.>`
 		// response back to Slack
 		res.send({text: searchText, channel: slackChannel, response_type: 'in_channel'})
 	})
