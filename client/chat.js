24d19040d39822a568dbf3b8a16a385f25c1b1cb
aperez25 <alice.perez25@gmail.com>
2017-06-18 21:13:11 -0700

    feat(site): initial styling

--- client/chat.js
+++ client/chat.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
 const Chat = () => (
-  <div>
-    <iframe
-      width="500"
-      height="600"
-      src="https://console.api.ai/api-client/demo/embedded/b77433ff-768e-41ff-a8a7-24fe2a5219b8" />
+  <div className="col-lg-6 chat">
+    <input id="speech" type="text" />
+    <button id="rec" class="btn">Speak</button>
+    <div id="spokenResponse" class="spoken-response" />
+      <div class="spoken-response__text" />
   </div>
 )
 
 export default Chat
+
+// <iframe
+//   width="500"
+//   height="600"
+// src="https://console.api.ai/api-client/demo/embedded/b77433ff-768e-41ff-a8a7-24fe2a5219b8" />
--- client/root.js
+++ client/root.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import About from './about'
 import Chat from './chat'
+import Slack from './slackbutton'
 
 const Root = () => (
-  <div>
+  <div className="row">
     <About />
     <Chat />
+    <Slack />
   </div>
 )
 
