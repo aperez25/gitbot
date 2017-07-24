9736125d8d833a884e2f9f790aeaf82d3a1c6910
Alice <ally@Alices-MacBook-Pro.local>
2017-07-24 11:06:14 -0700

    feat(gitCommands): dropdown menu rendering properly

--- server/api/site.js
+++ server/api/site.js
@@ -1,39 +1,39 @@
 'use strict'
 
 const router = require('express').Router()
-const apiai = require('apiai')
-const apiaiRouter = apiai(process.env.APIAI_CLIENT_ACCESS_TOKEN)
+// const apiai = require('apiai')
+// const apiaiRouter = apiai(process.env.APIAI_CLIENT_ACCESS_TOKEN)
 
-router
-.post('/', (req, res, next) => {
-  console.log(req)
-  ask(req.body.question)
-	.then(response => {
-		res.send(response.result.fulfillment.speech);
-	})
-  .catch(error => {
-		console.log(error)
-	});
-})
+// router
+// .post('/', (req, res, next) => {
+//   console.log(req)
+//   ask(req.body.question)
+// 	.then(response => {
+// 		res.send(response.result.fulfillment.speech);
+// 	})
+//   .catch(error => {
+// 		console.log(error)
+// 	});
+// })
 
 
-function ask(text, options) {
-	return new Promise((resolve, reject) => {
-		var defaultOptions = {
-			sessionId: Math.floor((Math.random() + 1) * 1000) // can use any arbitrary id
-		};
-		let request = apiaiRouter.textRequest(text, Object.assign(defaultOptions, options));
-		request.on('response', (response) => {
-			return resolve(response);
-		});
+// function ask(text, options) {
+// 	return new Promise((resolve, reject) => {
+// 		var defaultOptions = {
+// 			sessionId: Math.floor((Math.random() + 1) * 1000) // can use any arbitrary id
+// 		};
+// 		let request = apiaiRouter.textRequest(text, Object.assign(defaultOptions, options));
+// 		request.on('response', (response) => {
+// 			return resolve(response);
+// 		});
 
-		request.on('error', (error) => {
-			return reject(error);
-		});
+// 		request.on('error', (error) => {
+// 			return reject(error);
+// 		});
 
-		request.end();
-	})
-}
+// 		request.end();
+// 	})
+// }
 // ask something
 
 // function getAllIntents(options) {
