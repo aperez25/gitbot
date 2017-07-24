381dcdfa0cb543b875afe97cb0a5e1dd3ab46fbf
Alice <ally@Alices-MacBook-Pro.local>
2017-07-24 11:24:32 -0700

    style(gitCommands): centered dropdown bar

--- client/commands.js
+++ client/commands.js
@@ -2,18 +2,18 @@ import React from 'react'
 import { DropdownButton, MenuItem, FormGroup, FormControl } from 'react-bootstrap'
 
 const Commands = () => (
-<div>
+<div id="dropdownmenu">
    <form>
-      <div className = "row" id="dropdownmenu">
-         <div className = "col-lg-6">
+      <div className = "row">
+         <div className = "col-lg-offset-3 col-lg-6">
             <div className = "input-group">
                <div className = "input-group-btn">
                   <DropdownButton title="GitBot Commands">
                      <span className = "caret"></span>
-                     <MenuItem><a href = "/api/slack/searchrepos">Search GitHub Repo's</a></MenuItem>
-                     <MenuItem><a href = "/api/slack/popular">Popular Repos</a></MenuItem>
-                     <MenuItem><a href = "/api/slack/gitbranches">Active Repo Branches</a></MenuItem>
-                     <MenuItem><a href = "/api/slack/gitcommit">Last Repo Commit</a></MenuItem>
+                     <MenuItem><a className="dropdownLink" href = "/api/slack/searchrepos">Search GitHub Repo's</a></MenuItem>
+                     <MenuItem><a className="dropdownLink" href = "/api/slack/popular">Popular Repos</a></MenuItem>
+                     <MenuItem><a className="dropdownLink" href = "/api/slack/gitbranches">Active Repo Branches</a></MenuItem>
+                     <MenuItem><a className="dropdownLink" href = "/api/slack/gitcommit">Last Repo Commit</a></MenuItem>
                   </DropdownButton>
                </div>
                <FormControl type = "text" placeholder="Enter text" />
