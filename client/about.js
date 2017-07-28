5b0246ac6c98060592daed365b0ca107b907c8e7
Alice <aliceperez25@gmail.com>
2017-07-28 16:36:23 -0700

    style updates to chat, renamed routes to be restful

--- client/about.js
+++ client/about.js
@@ -9,7 +9,7 @@ const About = () => (
         <ul>
         <li><Link to='/chatbox'>Chat in realtime with GitBot</Link> for simple Q&A</li>
         <li id="chatmarkdown"><Link to="/chat">Exchange messages with GitBot</Link> for workflow questions like "How do I create a new project?"</li>
-        <li id="commands"><Link to="/commands">Use GitBot commands</Link> to see what's happening on GitHub</li>
+        <li id="commands"><Link to="/commands">Preview the Slack commands</Link> with some cool gifs</li>
         </ul>
       </p>
     </div>
